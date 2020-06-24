148150d04dc03c129cebbf85af0898c9b7417fbd
Alan Choi <choisteelbear@gmail.com>
2020-06-24 23:46:16 +0900

    solved ~1.28 & complete 1.1 (really)

--- chapter1/1-2.rkt
+++ chapter1/1-2.rkt
@@ -183,10 +183,11 @@
 (define (smallest-divisor n)
   (find-divisor n 2))
 
-(define (find-divisor n test-divisor)
-  (cond ((> (square test-divisor) n) n)
-        ((divides? test-divisor n) test-divisor)
-        (else (find-divisor n (+ test-divisor 1)))))
+; commented for 1.23
+;(define (find-divisor n test-divisor)
+;  (cond ((> (square test-divisor) n) n)
+;        ((divides? test-divisor n) test-divisor)
+;        (else (find-divisor n (+ test-divisor 1)))))
 
 (define (divides? a b)
   (= (remainder b a) 0))
@@ -194,14 +195,15 @@
 (define (prime? n)
   (= n (smallest-divisor n)))
 
-(define (expmod base exp m)
-  (cond ((= exp 0) 1)
-        ((even? exp)
-         (remainder (square (expmod base (/ exp 2) m))
-                    m))
-        (else
-         (remainder (* base (expmod base (- exp 1) m))
-                    m))))
+; commented for 1.28
+;(define (expmod base exp m)
+;  (cond ((= exp 0) 1)
+;        ((even? exp)
+;         (remainder (square (expmod base (/ exp 2) m))
+;                    m))
+;        (else
+;         (remainder (* base (expmod base (- exp 1) m))
+;                    m))))
 
 (define (fermat-test n)
   (define (try-it a)
@@ -228,9 +230,10 @@
 (define (timed-prime-test n)
   (start-prime-test n (runtime)))
 
-(define (start-prime-test n start-time)
-  (if (prime? n)
-      (report-prime n (- (runtime) start-time))))
+; commented for 1.24
+;(define (start-prime-test n start-time)
+;  (if (prime? n)
+;      (report-prime n (- (runtime) start-time))))
 
 (define (report-prime n elapsed-time)
   (newline)
@@ -241,12 +244,10 @@
 (define (search-for-primes start end)
   (define (search-n-back n)
     (timed-prime-test n)
-    (search-for-primes (+ n 2) end))
-  (if (even? start)
-      (search-for-primes (+ start 1) end)
-      (if (or (< start end)
-              (= start end))
-          (search-n-back start))))
+    (search-for-primes (+ n 1) end))
+  (if (or (< start end)
+          (= start end))
+    (search-n-back start)))
 
 ;1009 *** 0
 ;1013 *** 0
@@ -267,3 +268,131 @@
 ; 대부분 소수인지 판별하는데 1s미만이라 10^1/2인지 확인할 수 없음
 ; 다만 어떤 소수는 1000이 나오기도 하며 이는 일정치 않음
 ; 그렇기에 항상 시간이 O(n^1/2)를 완전히 따른다고 할 수 없음.
+
+;; exercise 1.23
+(define (next n)
+  (if (= n 2)
+      3
+      (+ n 2)))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (next test-divisor)))))
+
+; 이 문제 또한 각 소수들을 구하는 시간이 너무 짧아
+; 정확하게 측정하기는 어렵다.
+
+;; exercise 1.24
+
+(define (start-prime-test n start-time)
+  (if (fast-prime? n 3)
+      (report-prime n (- (runtime) start-time))))
+
+; 전부 0으로 나와 측정 불가
+
+;; exercise 1.25
+; 만약 이 언어가 표현할 수 있는 정수에 한계가 있다면
+; 1.25에서 정의한 방법대로 계산할 수 없다.
+; 특히 지수 계산이기 때문에
+; 조금만 수를 올려도 한계치에 도달할 수 있다.
+; 그렇기 때문에 계산할 값의 크기를 줄여
+; 이런 문제를 예방하는 방법이 더 좋다.
+
+;; exercise 1.26
+; 이 프로시저는 exp가 짝수인 경우 (/ exp 2)식을 통해
+; 계산할 때 필요한 exp 수를 n에서 log_2(n)으로 줄였다.
+; 하지만 이러한 재귀식을 2번쓰게되면
+; 같은 계산을 2번하게되므로
+; n에서 2^n으로 expmod의 호출량이 많아지게 된다.
+; 결국 Loise가 쓴 정의대로 프로시저를 만들면
+; 계산하는 시간이 O(n)을 따라간다.
+
+;; exercise 1.27
+(define (test-fermat n)
+  (define (modulo-check a n)
+    (= (expmod a n n) a))
+  (define (modulo-filter a n)
+    (if (not (modulo-check a n))
+        (display-number a)))
+  (define (display-number n)
+    (display n)
+    (newline))
+  (define (iter i a)
+    (if (< i a)
+        (run-and-back i a)))
+  (define (run-and-back i a)
+    (modulo-filter i n)
+    (iter (+ i 1) a))
+  (iter 1 n))
+
+; test-fermat은 페르마 검사에서 조건을 만족하지 않는 a값을 보여준다.
+; 즉, 결과가 하나라도 나온다면 합성수
+; 결과가 나오지 않는다면 소수여야 한다. 하지만,
+
+; > (smallest-divisor 561)
+; 3
+; > (test-fermat 561)
+; >
+
+; > (smallest-divisor 1105)
+; 5
+; > (test-fermat 1105)
+; >
+
+; > (smallest-divisor 1729)
+; 7
+; > (test-fermat 1729)
+; >
+
+; > (smallest-divisor 2465)
+; 5
+; > (test-fermat 2465)
+; >
+
+; > (smallest-divisor 2821)
+; 7
+; > (test-fermat 2821)
+; >
+
+; > (smallest-divisor 6601)
+; 7
+; > (test-fermat 6601)
+; >
+
+; 각주 47에서 나온 카마이클 수 561, 1105, 2465, 2821, 6601은
+; 엄연히 합성수임에도 페르마 검사를 만족한다.
+
+;; exercise 1.28
+(define (expmod base exp m)
+  (cond ((or (= base 1)
+             (= base (- exp 1)))
+         0)
+        ((= exp 0) 1)
+        ((even? exp)
+         (remainder (square (expmod base (/ exp 2) m))
+                    m))
+        (else
+         (remainder (* base (expmod base (- exp 1) m))
+                    m))))
+
+(define (ml-test n)
+  (define (try-it a)
+    (= (expmod a (- n 1) n) 1))
+  (try-it (+ 1 (random (- n 1)))))
+
+; 위의 코드는 fermat-test를 변형해서 작성했다.
+; 밀러-라빈 검사의 조건을 이용하여 확실하게 검사하도록 짠다면,
+
+(define (ml-prime? n)
+  (define (limit-of-test)
+    (+ (/ (- n (remainder n 2)) 2) 2))
+  (define (ml-check a)
+    (define (try-it a)
+      (not (= (expmod a (- n 1) n) 1)))
+    (try-it a))
+  (define (iter i limit)
+    (cond ((> i limit) #f)
+          ((ml-check i) (iter (+ i 1) limit))
+          (else #t)))
+  (iter 2 (limit-of-test)))
