befa4a1ebe42dbe737efe2bdca41d45fff83ad59
Alan Choi <choisteelbear@gmail.com>
2020-07-01 23:51:16 +0900

    solved ~2.15 & skip 2.16

--- chapter2/chapter2-1.rkt
+++ chapter2/chapter2-1.rkt
@@ -173,6 +173,11 @@
     (make-interval (min p1 p2 p3 p4)
                    (max p1 p2 p3 p4))))
 
+(define (div-interval x y)
+  (mul-interval x
+                (make-interval (/ 1.0 (upper-bound y))
+                               (/ 1.0 (lower-bound y)))))
+
 ;; exercise 2.7
 (define (make-interval a b) (cons a b))
 
@@ -231,3 +236,60 @@
                         (* (lower-bound a) (upper-bound b))))
         (else (make-interval (* (lower-bound a) (lower-bound b))
                              (* (upper-bound a) (upper-bound b))))))
+
+;; 2.1.4 (continue)
+(define (make-center-width c w)
+  (make-interval (- c w) (+ c w)))
+
+(define (center i)
+  (/ (+ (lower-bound i) (upper-bound i)) 2))
+
+(define (width i)
+  (/ (- (upper-bound i) (lower-bound i)) 2))
+
+;; exercise 2.12
+(define (make-center-percent c p)
+  (make-interval (- c (* c p)) (+ c (* c p))))
+
+(define (percent i)
+  (define center
+    (/ (+ (upper-bound i) (lower-bound i)) 2.0))
+  (/ (- (upper-bound i) center)
+     center))
+
+;; exercise 2.13
+(define (percent-xy x y)
+  (/ (+ (percent x) (percent y))
+     (+ 1
+        (* (percent x) (percent y)))))
+; lower-x > 0, lower-y > 0
+
+;; exercise 2.14
+(define (par1 r1 r2)
+  (div-interval (mul-interval r1 r2)
+                (add-interval r1 r2)))
+
+(define (par2 r1 r2)
+  (let ((one (make-interval 1 1)))
+    (div-interval one
+                  (add-interval (div-interval one r1)
+                                (div-interval one r2)))))
+
+; > (define A (make-center-percent 1 0.02))
+; > (define B (make-center-percent 5 0.01))
+
+; > (percent (par1 A A))
+; 0.05993607670795057
+; > (percent (par2 A A))
+; 0.020000000000000018
+; > (percent (par1 A B))
+; 0.04164609464794373
+; > (percent (par2 A B))
+; 0.01833375006250944
+
+;; exercise 2.15
+; 두 범위를 어떤 연산에 넣으면 오차는 커진다.
+; 그렇기에 범위 값을 최대한 쓰지않는 공식이 오차가 적다.
+
+;; exercise 2.16
+; need help
